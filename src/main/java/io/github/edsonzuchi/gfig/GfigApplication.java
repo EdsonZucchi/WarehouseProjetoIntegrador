d56a7011d8f3453d8cd331369fd5e0e488c85e27
Edson Zucchi <edsonluiz.zuchi@gmail.com>
2024-09-13 19:38:47 -0300

    first commit

---
+++ src/main/java/io/github/edsonzuchi/gfig/GfigApplication.java
@@ -0,0 +1,13 @@
+package io.github.edsonzuchi.gfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GfigApplication.class, args);
+    }
+
+}
